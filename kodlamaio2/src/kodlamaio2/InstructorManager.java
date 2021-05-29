ef121254e08e426ddc14b0034c536fed82832c48
tulin <40441327+tulinaktas@users.noreply.github.com>
2021-05-29 04:12:28 +0300

    Delete kodlamaio2 directory

--- kodlamaio2/src/kodlamaio2/InstructorManager.java
+++
@@ -1,7 +0,0 @@
-package kodlamaio2;
-
-public class InstructorManager extends UserManager{
-	public void getCourse(Instructor instructor) {
-		System.out.println(instructor.getCourse());
-	}
-}
--- kodlamaio2/src/kodlamaio2/StudentManager.java
+++
@@ -1,7 +0,0 @@
-package kodlamaio2;
-
-public class StudentManager extends UserManager {
-	public void getTrainingCourse(Student student) {
-		System.out.println(student.getTrainingCourse());
-	}
-}
